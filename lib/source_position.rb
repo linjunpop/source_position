92887cb4764f93730c9fdbd7e81b54600ec607e6
Jun Lin <linjunpop@gmail.com>
2012-07-09 16:18:01 +0800

    fixes #1 nil value of source_location

--- lib/source_position.rb
+++ lib/source_position.rb
@@ -2,7 +2,7 @@ require "source_position/version"
 
 module SourcePosition
   def source_position
-    self.source_location.join(':')
+    self.source_location.join(':') if self.source_location
   end
 end
 
--- spec/source_position_spec.rb
+++ spec/source_position_spec.rb
@@ -11,5 +11,10 @@ describe SourcePosition do
   subject { Dummy.method(:foobar) }
 
   its(:source_position) { should eq "#{Dir.pwd}/spec/source_position_spec.rb:5"}
+
+  context 'source_location returns nil value' do
+    subject { Object.method(:to_s) }
+    its(:source_position) { should eq nil }
+  end
 end
 
